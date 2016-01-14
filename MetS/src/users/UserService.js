8ccb3945471abfc1912379d971f5685201521883
Jose Checa Claudel <jcheca@uco.es>
2016-01-14 22:23:53 +0100

    Add MetS Version 0 - Demo

---
+++ MetS/src/users/UserService.js
@@ -0,0 +1,58 @@
+(function(){
+  'use strict';
+
+  angular.module('users')
+         .service('userService', ['$q', UserService]);
+
+  /**
+   * Users DataService
+   * Uses embedded, hard-coded data model; acts asynchronously to simulate
+   * remote data service call(s).
+   *
+   * @returns {{loadAll: Function}}
+   * @constructor
+   */
+  function UserService($q){
+    var users = [
+      {
+        name: 'Manuel Romero-Saldaña',
+        avatar: 'svg-1',
+        content: 'Metabolic syndrome (MetS) is defined as a group of meta- bolic risk factors, characterised <br>by central obesity, high arterial blood pressure (BP) and an impaired glucose (hyperglycaemia) and lipid (hypertriglyceridaemia and low high-density lipoprotein (HDL)-cholesterol) metabolism.1 MetS is closely linked to type 2 diabetes mellitus (DM), cardiovascular disease (CVD) and other cardiovascular risk factors that increase morbidity and mortality.2,3 The general increase in the prevalence of MetS over the past few decades has made it an urgent priority for public health services in all countriesspecially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.'
+      },
+      {
+        name: 'Francisco J Fuentes-Jiménez',
+        avatar: 'svg-2',
+        content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
+      },
+      {
+        name: 'Manuel Vaquero-Abellán',
+        avatar: 'svg-9',
+        content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
+      },
+      {
+        name: 'Carlos Álvarez-Fernández',
+        avatar: 'svg-4',
+        content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
+      },
+      {
+        name: 'Guillermo Molina-Recio',
+        avatar: 'svg-5',
+        content: 'Webtwo ipsum dolor sit amet, eskobo chumby doostang bebo. Bubbli greplin stypi prezi mzinga heroku wakoopa, shopify airbnb dogster dopplr gooru jumo, reddit plickers edmodo stypi zillow etsy.'
+      },
+      {
+        name: 'José López-Miranda',
+        avatar: 'svg-6',
+        content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
+      }
+    ];
+
+    // Promise-based API
+    return {
+      loadAllUsers : function() {
+        // Simulate async nature of real remote calls
+        return $q.when(users);
+      }
+    };
+  }
+
+})();
