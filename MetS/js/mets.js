78d36b56170115978875103ae7ba755f86913823
Jose Checa Claudel <jcheca@uco.es>
2016-02-25 18:07:21 +0100

    Actualizando Script Idioma LocalStorage

--- MetS/js/mets.js
+++ MetS/js/mets.js
@@ -1,4 +1,24 @@
 angular.module("mets", [])
-    .controller("Idioma", function($scope) {
-        $scope.Idioma = "ES";
-    } );
+   .controller("Idioma", function($scope) {
+
+    /* Idioma por defecto: EN */
+    if (localStorage.getItem("Idioma") === null){
+        localStorage.setItem("Idioma","EN");
+        $scope.Idioma = "EN";
+    } else {
+        $scope.Idioma = localStorage.getItem("Idioma");
+    }
+
+    /* Switch Idioma */
+    $scope.cambiame = function()  {
+        if (localStorage.getItem("Idioma") == "EN"){
+            localStorage.setItem("Idioma","ES");
+            $scope.Idioma = "ES";
+        } else {
+            localStorage.setItem("Idioma","EN");
+            $scope.Idioma = "EN";
+        }
+    }    
+
+
+});
