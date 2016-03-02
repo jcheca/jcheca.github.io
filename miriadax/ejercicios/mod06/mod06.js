22741e76b6406cbd26bd781e83d4ee71df6a377d
Jose Checa Claudel <jcheca@uco.es>
2016-03-02 21:04:15 +0100

    Añadido Video

--- miriadax/ejercicios/mod06/mod06.js
+++
@@ -1,8 +0,0 @@
-var tabla = document.getElementById("tabla");
-var i=0;
-
-for (i in document.location) {	
-	if (typeof(document.location[i]) !== "string") value = "no imprimible"; else value = document.location[i];	
-	tabla.innerHTML+="<tr><td>"+i+"</td><td> = "+value+"</td></tr>";
-}
-
--- miriadax/notifica/Pocket-Kitten/README.md
+++
@@ -1,6 +0,0 @@
-Pocket-Kitten
-=============
-
-A simple Firefox OS app for Gaia
-
-[View it in action here](http://rumyra.github.com/Pocket-Kitten/index.html)
